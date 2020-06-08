a335a58a982dc12c7cf43ae25179bc8b28c84bcb
张立新 <zhang_lx@suixingpay.com>
2020-06-08 16:08:38 +0800

    可重入锁测试

---
+++ service/src/test/java/com/model/zlx/zhanglxalex/locktest/LockTests.java
@@ -0,0 +1,65 @@
+package com.model.zlx.zhanglxalex.locktest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 可重入锁的公平锁与非公平锁的测试
+ * 结论：公平性锁每次都是从同步队列中的第一个节点获取到锁，而非公平性锁出现了一个线程连续获取锁的情况。
+ */
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class LockTests {
+
+    private static Lock fairLock = new ReentrantLock2(true);
+    private static Lock unfairLock = new ReentrantLock2(false);
+
+    @Test
+    public void fair() {
+        testLock(fairLock);
+    }
+
+    @Test
+    public void unfair() {
+        testLock(unfairLock);
+    }
+
+    private void testLock(Lock lock) {
+        // 启动5个Job（略）
+    }
+
+    private static class Job extends Thread {
+        private Lock lock;
+
+        public Job(Lock lock) {
+            this.lock = lock;
+        }
+
+        public void run() {
+            // 连续2次打印当前的Thread和等待队列中的Thread（略）
+        }
+    }
+
+    private static class ReentrantLock2 extends ReentrantLock {
+        public ReentrantLock2(boolean fair) {
+            super(fair);
+        }
+
+        public Collection<Thread> getQueuedThreads() {
+            List<Thread> arrayList = new ArrayList<Thread>(super.
+                    getQueuedThreads());
+            Collections.reverse(arrayList);
+            return arrayList;
+        }
+    }
+
+}
